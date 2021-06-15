e20a0f0e48990e9881cd1591dd4f2a04034dbb77
Wing Sang Vincent Liu <wingsangvincentliu@gmail.com>
2021-06-15 15:50:49 +0100

    added onDrag callback

--- dist/BottomSheet.d.ts
+++ dist/BottomSheet.d.ts
@@ -23,4 +23,5 @@ export declare const BottomSheet: React.ForwardRefExoticComponent<{
     reserveScrollBarGap?: boolean;
     skipInitialTransition?: boolean;
     disableDrag?: boolean;
+    onDrag?: (state: import("react-use-gesture/dist/types").Handler<"drag", React.PointerEvent<Element> | PointerEvent>) => {};
 } & Omit<Pick<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, "key" | keyof React.HTMLAttributes<HTMLDivElement>>, "children"> & React.RefAttributes<RefHandles>>;
